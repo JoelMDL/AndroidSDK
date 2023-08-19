adafb7624015ce58941d659a9714378d2ba93a1c
JoelMD <joelmidilli@gmail.com>
2023-08-20 00:26:35 +0300

    update

--- BalanceApp/app/src/main/java/com/test/balanceapp/MainActivity.java
+++ BalanceApp/app/src/main/java/com/test/balanceapp/MainActivity.java
@@ -3,12 +3,28 @@ package com.test.balanceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button plusBtn;
+    private Button minusBtn;
+    double balance = 0;
+    TextView balanceNum = findViewById(R.id.balanceNum);
+    EditText balanceInput = findViewById(R.id.balanceInput);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void plusClick(View view) {
+
+        balanceNum.setText("Hello " + balanceInput);
+    }
+
 }
--- BalanceApp/settings.gradle.kts
+++ BalanceApp/settings.gradle.kts
@@ -15,4 +15,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "BalanceApp"
 include(":app")
- 
