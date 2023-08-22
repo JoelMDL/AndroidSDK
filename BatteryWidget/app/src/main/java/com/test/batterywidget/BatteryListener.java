3cadf6d38b739d49ad195c904b249e5c151aad62
JoelMD <joelmidilli@gmail.com>
2023-08-23 00:10:42 +0300

    update

---
+++ BatteryWidget/app/src/main/java/com/test/batterywidget/BatteryListener.java
@@ -0,0 +1,88 @@
+package com.test.batterywidget;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.IBinder;
+import android.appwidget.AppWidgetManager;
+import android.view.View;
+import android.widget.RemoteViews;
+
+public class BatteryListener extends Service {
+
+    private BroadcastReceiver batteryReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction() != null && intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)) {
+                updateWidgets(context);
+            }
+        }
+    };
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(batteryReceiver, intentFilter);
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void updateWidgets(Context context) {
+        int batteryPercentage = getBatteryPercentage(context);
+        boolean isCharging = isCharging(context);
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        int[] widgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(context, BatteryWidgetProvider.class));
+
+        for (int appWidgetId : widgetIds) {
+            updateWidget(context, appWidgetId, batteryPercentage, isCharging);
+        }
+    }
+
+    private boolean isCharging(Context context) {
+        Intent batteryStatus = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        return status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL;
+    }
+
+
+
+    private int getBatteryPercentage(Context context) {
+        Intent batteryStatus = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        if (scale != 0) {
+            return (int) ((level / (float) scale) * 100);
+        } else {
+            return 0;
+        }
+    }
+
+    private void updateWidget(Context context, int appWidgetId, int batteryLevel, boolean isCharging) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+
+        if (isCharging) {
+            views.setViewVisibility(R.id.phoneIco, View.VISIBLE); // Hide phone icon
+            views.setViewVisibility(R.id.chargeIcon, View.VISIBLE); // Show charging icon
+            views.setTextViewText(R.id.batteryPercentage, ""); // Clear the text
+        } else {
+            views.setViewVisibility(R.id.phoneIco, View.VISIBLE); // Show phone icon
+            views.setViewVisibility(R.id.chargeIcon, View.GONE); // Hide charging icon
+            views.setTextViewText(R.id.batteryPercentage, batteryLevel + "%");
+        }
+
+        views.setProgressBar(R.id.batteryBar, 100, batteryLevel, false);
+        AppWidgetManager.getInstance(context).updateAppWidget(appWidgetId, views);
+    }
+
+
+
+}
--- BatteryWidget/app/src/main/java/com/test/batterywidget/BatteryWidgetProvider.java
+++ BatteryWidget/app/src/main/java/com/test/batterywidget/BatteryWidgetProvider.java
@@ -2,10 +2,8 @@ package com.test.batterywidget;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.BatteryManager;
 import android.widget.RemoteViews;
 
@@ -18,34 +16,19 @@ public class BatteryWidgetProvider extends AppWidgetProvider {
         if (intent.getAction() != null && intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)) {
             int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
             if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
-                updateBatteryWidget(context, AppWidgetManager.getInstance(context), appWidgetId);
+                // No need to call updateWidget here
+                // Start the BatteryService to handle battery updates
+                Intent serviceIntent = new Intent(context, BatteryListener.class);
+                context.startService(serviceIntent);
             }
         }
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for (int appWidgetId : appWidgetIds) {
-            updateBatteryWidget(context, appWidgetManager, appWidgetId);
-        }
-    }
-
-    private void updateBatteryWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-        int batteryPercentage = getBatteryPercentage(context);
-        views.setProgressBar(R.id.batteryBar, 100, batteryPercentage, false);
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
-
-    private int getBatteryPercentage(Context context) {
-        Intent batteryStatus = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-
-        if (scale != 0) {
-            return (int) ((level / (float) scale) * 100);
-        } else {
-            return 0;
-        }
+        // Update battery widget directly from BatteryService
+        // Start the BatteryService to handle battery updates
+        Intent serviceIntent = new Intent(context, BatteryListener.class);
+        context.startService(serviceIntent);
     }
 }
